2bd5373a9bcccc0e523a65356318fbe6db33842a
FancyCodeMaster <fancycodemaster@gmail.com>
2021-03-06 23:17:07 +0545

    adding functionalities

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const cartBtn = document.querySelectorAll(".add-cart");
 const cartNum = document.querySelector(".cart-number");
 const navBar = document.querySelector("nav");
+const btn = document.querySelector("button");
 
 
 let collections = {
@@ -20,4 +21,8 @@ for (let i = 0 ; i<cartBtn.length ; i++) {
 
 document.addEventListener("scroll" , () => {
     navBar.classList.toggle("sticky" , window.scrollY > 100);
+})
+
+btn.addEventListener("click" , (event) => {
+    event.preventDefault();
 })
